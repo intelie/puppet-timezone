17890a160a4127d8c4d18479f0c5887fa75e31c9
Jorge Falcão <falcao@intelie.com.br>
2013-02-14 11:35:20 -0200

    first commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name    'intelie-timezone'
+version '0.0.1'
+source 'UNKNOWN'
+author 'Intelie'
+license 'Apache License, Version 2.0'
+summary 'Timezone configuration'
+description 'Module to handle timezone configuration'
+project_page 'UNKNOWN'
+
---
+++ manifests/init.pp
@@ -0,0 +1,43 @@
+# == Class: timezone
+#
+# Timezone configuration
+#
+# === Parameters
+#
+# [*zone*]
+#   Timezone name default: 'America/Sao_Paulo'
+##
+# === Examples
+#
+#  class { timezone:
+#    zone => 'America/Sao_Paulo'
+#  }
+#
+# === Authors
+#
+# Jorge Falcão <falcao@intelie.com.br>
+#
+# === Copyright
+#
+# Copyright 2012 Intelie
+#
+
+class timezone($zone = 'America/Sao_Paulo') {
+  case $operatingsystem {
+    centos, redhat: {
+      file { '/etc/sysconfig/clock':
+        ensure  => present,
+        content => 'ZONE="${zone}"',
+        } ~> exec { 'tzdata-update':
+          path        => '/usr/sbin/',
+          refreshonly => true
+        }
+    }
+    #debian, ubuntu: {
+    # TODO
+    #}
+    default: {
+      fail("Module ${module_name} is not supported on ${operatingsystem}")
+    }
+  }
+}
